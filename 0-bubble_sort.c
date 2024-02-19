cfc81d8a327733322c2af7a2b93615d675f2daf3
creih <iherr.9@gmail.com>
2024-02-19 09:44:04 +0200

    fixed index out of range and added terminating flag for once the array is sorted

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,26 +7,29 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    size_t index;
+    size_t index, tambuka;
     int tmp;
+    bool exch;
 
-    if (size < 2)
+    if (size < 2 || array == NULL)
     {
-        return "array too short";
+        return;
     }
-    for (index = 0; index <= size; index++)
+    for (tambuka = 0; tambuka < size - 1; tambuka ++)
     {
-        if (array[index] <= array[index + 1])
+        exch = false;
+        for (index = 0; index < size - 1 - tambuka; index++)
         {
-            continue;
+            if (array[index] > array[index + 1])
+            {
+                tmp = array[index];
+                array[index] = array[index + 1];
+                array[index + 1] = tmp;
+                print_array(array, size);
+                exch = true;
+            }
         }
-        else
-        {
-            tmp = array[index];
-            array[index] = array[index + 1];
-            array[index + 1] = tmp;
-        }
-        print_array(array, size);
+        if (exch == false)
+        break;
     }
 }
-// may need to print the array one last final time after the for loop
