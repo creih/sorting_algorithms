5dba6946dc49560f7417adb5799120e1d64e2e3b
creih <iherr.9@gmail.com>
2024-02-19 09:17:41 +0200

    task 0, and task 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,5 +7,26 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    return;
+    size_t index;
+    int tmp;
+
+    if (size < 2)
+    {
+        return "array too short";
+    }
+    for (index = 0; index <= size; index++)
+    {
+        if (array[index] <= array[index + 1])
+        {
+            continue;
+        }
+        else
+        {
+            tmp = array[index];
+            array[index] = array[index + 1];
+            array[index + 1] = tmp;
+        }
+        print_array(array, size);
+    }
 }
+// may need to print the array one last final time after the for loop
