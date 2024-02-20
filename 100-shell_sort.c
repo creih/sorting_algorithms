c774b450c8695cc6223fd82170cb566b97eb5c33
creih <iherr.9@gmail.com>
2024-02-20 13:47:26 -0800

    task 100

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -6,22 +6,25 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	    size_t gap = 1;
-	        size_t i, j;
+	size_t gap = 1;
+	size_t i, j;
+	int temp;
 
-    while (gap < size / 3)
-    gap = gap * 3 + 1;
-    for (; gap > 0; gap /= 3)
-    {
-        for (i = gap; i < size; i++)
-        {
-            int temp = array[i];
-            for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-            {
-                array[j] = array[j - gap];
-            }
-            array[j] = temp;
-        }
-        print_array(array, size);
-    }
+	while (gap < size / 3)
+	{
+		gap = gap * 3 + 1;
+		for (; gap > 0; gap /= 3)
+		{
+			for (i = gap; i < size; i++)
+			{
+				temp = array[i];
+				for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+				{
+					array[j] = array[j - gap];
+				}
+				array[j] = temp;
+			}
+		print_array(array, size);
+		}
+	}
 }
