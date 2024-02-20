df7ca9e83eb060713b11f5e82c0527c27d1217fe
creih <iherr.9@gmail.com>
2024-02-20 12:22:36 -0800

    this is task 3

--- sort.h
+++ sort.h
@@ -25,5 +25,9 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void swap(int *one, int *two);
+void partition(int *array, int low, int high, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
